5e018cc3310ec1fe626d588951e35c448b0f4740
Rewax <daniel.lorenzen@hotmail.com>
2021-05-01 21:44:02 +0200

    Maybe done

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -15,7 +15,7 @@ const requestOptions = {
 
 
 
-data = {
+data = [{
     id : 26952557,
     ranking : 1,
     link : "https://www.matuzo.at/blog/html-boilerplate/",
@@ -36,7 +36,7 @@ data = {
         postedBy: "anders_dk",
         postTime: "6 hours ago",
         amountOfComments: 146
-}
+}]
 
 //======== FETCH Post ========
 // fetch(myUrl, requestOptions)
@@ -46,37 +46,44 @@ data = {
 //         console.log(data)
 //     })
 
-fillPostHeader(data)
+console.log(data)
+data.forEach(fillPostHeader)
 
-function fillPostHeader(item) {
+function fillPostHeader(item,index) {
     console.log(item)
     const headerParent = document.querySelector('.postHeader')
+
+    // === CREATE atag ===
+    let a = document.createElement('a')
+    a.setAttribute("href", item.link)
+    a.classList.add("link")
+    headerParent.appendChild(a)
 
     // === CREATE h4 ===
     let h4 = document.createElement('h4')
     h4.textContent = item.title;
-    headerParent.appendChild(h4)
+    a.appendChild(h4)
     console.log(item.title)
 
 
     // === CREATE span ===
     let span = document.createElement('span')
-    span.textContent = item.points;
+    span.textContent = item.points + " Points ";
     headerParent.appendChild(span)
 
     // === CREATE atag ===
-    let a = document.createElement('a')
-    a.textContent = item.postedBy;
-    headerParent.appendChild(a)
+    let a1 = document.createElement('a')
+    a1.textContent = " by " + item.postedBy;
+    headerParent.appendChild(a1)
 
     // === CREATE span ===
     let span2 = document.createElement('span')
-    span2.textContent = item.postTime;
+    span2.textContent = " " + item.postTime + " ";
     headerParent.appendChild(span2)
 
     // === CREATE atag ===
     let a2 = document.createElement('a')
-    a2.textContent = item.amountOfComments;
+    a2.textContent = item.amountOfComments + " comments";
     headerParent.appendChild(a2)
 
 }
