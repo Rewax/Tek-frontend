16acf40cc4a30b37cb500f93cb60005d0cfde235
Rewax <daniel.lorenzen@hotmail.com>
2021-05-03 20:24:46 +0200

    V1 done

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,53 +1,42 @@
-const myUrl = 'http://localhost:8081/news';
-
+const myUrl = 'http://54.88.29.127:8082/news';
 const requestOptions = {
     'content-type': 'application/json',
     method: 'GET',
     redirect: 'follow'
 };
 
-// fetch(myUrl, requestOptions)
-//     .then(response => response.json())
-//     .then(data => {
-//         data.forEach(fillPostHeader())
-//         console.log(data)
-//     })
+fetch(myUrl, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(fillPostHeader)
+        console.log(data)
+    })
 
 
 
-data = [{
-    id : 26952557,
-    ranking : 1,
-    link : "https://www.matuzo.at/blog/html-boilerplate/",
-    title : "My Current HTML Boilerplate",
-    siteName : "matuzo.at",
-    points : 481,
-    postedBy : "adrian_mrd",
-    postTime : "6 hours ago",
-    amountOfComments : 146
-    },
-    {
-        id : 26952558,
-        ranking : 2,
-        link: "https://mars26.com/",
-        title: "Show HN: Open-source high-res Mars map, in natural colors",
-        siteName: "matuzo.at",
-        points: 900,
-        postedBy: "anders_dk",
-        postTime: "6 hours ago",
-        amountOfComments: 146
-}]
+// data = [{
+//     id : 26952557,
+//     ranking : 1,
+//     link : "https://www.matuzo.at/blog/html-boilerplate/",
+//     title : "My Current HTML Boilerplate",
+//     siteName : "matuzo.at",
+//     points : 481,
+//     postedBy : "adrian_mrd",
+//     postTime : "6 hours ago",
+//     amountOfComments : 146
+//     },
+//     {
+//         id : 26952558,
+//         ranking : 2,
+//         link: "https://mars26.com/",
+//         title: "Show HN: Open-source high-res Mars map, in natural colors",
+//         siteName: "matuzo.at",
+//         points: 900,
+//         postedBy: "anders_dk",
+//         postTime: "6 hours ago",
+//         amountOfComments: 146
+// }]
 
-//======== FETCH Post ========
-// fetch(myUrl, requestOptions)
-//     .then(response => response.json())
-//     .then(data  => {
-//         fillPostHeader(data)
-//         console.log(data)
-//     })
-
-console.log(data)
-data.forEach(fillPostHeader)
 
 function fillPostHeader(item,index) {
     console.log(item)
